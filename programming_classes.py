7af52eb1830a52e466ef62e9748d183eaa99571d
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-10-05 18:23:25 -0500

    Add files via upload

--- programming_classes.py
+++ programming_classes.py
@@ -1,3 +1,7 @@
+"""
+Now, let's practice using tuples. Imagine you're planning the classes for a programming certificate.
+You need to list out six classes. Here's what you need to do:
+"""
 def main():
     programming_classes = ("Intro to Python", "Advanced Python", "Database Essentials", "Web Development Basics", "Data Structures in Python", "Web Design Fundamentals")
     for item in programming_classes:
